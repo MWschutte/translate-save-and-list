a343845a25032d74f2ce25225da7519816eb67a9
marcu <marcus.schutte@live.nl>
2023-10-02 13:41:59 +0200

    Add filter to language picker.

--- lib/pages/select_language_page.dart
+++ lib/pages/select_language_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_translation/google_mlkit_translation.dart';
 
-class SelectLanguagePage extends StatelessWidget {
+class SelectLanguagePage extends StatefulWidget {
   const SelectLanguagePage(
       {required this.selectedLanguage,
       required this.onLanguageSelect,
@@ -11,29 +11,48 @@ class SelectLanguagePage extends StatelessWidget {
   final TranslateLanguage selectedLanguage;
 
   @override
+  State<SelectLanguagePage> createState() => _SelectLanguagePageState();
+}
+
+class _SelectLanguagePageState extends State<SelectLanguagePage> {
+  String filterString = '';
+
+  @override
   Widget build(BuildContext context) {
-    List<TranslateLanguage> languages = TranslateLanguage.values;
+    List<TranslateLanguage> languages = TranslateLanguage.values
+        .where((element) => element.name.startsWith(filterString))
+        .toList();
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("Choose language"),
         ),
-        body: ListView.builder(
-            itemCount: languages.length,
-            itemBuilder: (context, index) {
-              TranslateLanguage language = languages[index];
-              return ListTile(
-                leading: language == selectedLanguage
-                    ? const Icon(Icons.check)
-                    : Container(
-                        width: 10,
-                      ),
-                title: Text(languages[index].name),
-                onTap: () {
-                  onLanguageSelect(languages[index]);
-                  Navigator.of(context).pop();
-                },
-              );
-            }));
+        body: Column(children: [
+          TextField(
+              onChanged: (value) => setState(() {
+                    filterString = value;
+                  }),
+              decoration: const InputDecoration(
+                  hintText: 'Search in list',
+                  contentPadding: EdgeInsets.all(8),
+                  suffixIcon: Icon(Icons.search))),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: languages.length,
+                  itemBuilder: (context, index) {
+                    TranslateLanguage language = languages[index];
+                    return ListTile(
+                        leading: language == widget.selectedLanguage
+                            ? const Icon(Icons.check)
+                            : Container(
+                                width: 10,
+                              ),
+                        title: Text(languages[index].name),
+                        onTap: () {
+                          widget.onLanguageSelect(languages[index]);
+                          Navigator.of(context).pop();
+                        });
+                  }))
+        ]));
   }
 }
